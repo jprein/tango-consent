a0077334048e649eff537245c4e3fd3e5e37ce62
Julia Prein <26962518+jprein@users.noreply.github.com>
2025-05-15 11:37:53 +0200

    Add json upload for timestamped consent

--- src/js/instructions.js
+++ src/js/instructions.js
@@ -3,9 +3,46 @@ import '../css/style.css';
 const button = document.getElementById('continue-button');
 const checkbox = document.getElementById('confirm-checkbox');
 
-// If the checkbox is not checked, disable the button
+// Needs JSON.parse to convert the string back to an object, otherwise we get e.g. "\"test\""
+const subjID = JSON.parse(localStorage.getItem('subjID')) || 'test';
+
+// function for response logging, creating json file on server
+function uploadData(safe, ID) {
+  fetch('data/data.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ data: JSON.stringify(safe), fname: ID }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
 const handleChecked = () => {
+  // If the checkbox is not checked, disable the button
   button.disabled = !checkbox.checked;
+
+  console.log(checkbox.checked);
+
+  if (checkbox.checked) {
+    const date = new Date();
+
+    const toSave = {
+      // get ID out of URL parameter
+      subjID: subjID,
+      consent: true,
+      timestamp: date.toISOString(),
+      epoch: date.getTime(),
+    };
+    const toSaveID = `${subjID}`;
+    uploadData(toSave, toSaveID);
+  }
 };
 
 checkbox.addEventListener('change', handleChecked, { capture: false });
