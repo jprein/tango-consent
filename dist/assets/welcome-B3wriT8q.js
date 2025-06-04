5a2a77de3ccf0f865beb0b7d470962c4f5d14c6f
Julia Prein <26962518+jprein@users.noreply.github.com>
2025-06-04 13:11:48 +0200

    Commit dist for flexible deployment

---
+++ dist/assets/welcome-B3wriT8q.js
@@ -0,0 +1,1 @@
+import"./style-HX_bqIYk.js";const n=document.getElementById("continue-button"),e=t=>{t.preventDefault(),window.location.href="./instructions.html"};n.addEventListener("click",e,{capture:!1});
